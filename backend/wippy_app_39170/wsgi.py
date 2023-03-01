ce30c373de8b2049b5da477b3455693baff1905f
Crowdbotics <team@crowdbotics.com>
2023-03-01 04:36:25 +0000

    Initial commit.

---
+++ backend/wippy_app_39170/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for wippy_app_39170 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wippy_app_39170.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wippy-app-39170",
+  url: "https://wippy-app-39170.botics.co",
+  api: "https://wippy-app-39170.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
